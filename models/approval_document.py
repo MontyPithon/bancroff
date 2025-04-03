682282dd3cfe20f8cff43f4cd18528e72f34dc99
Andy Tran <andytran557@gmail.com>
2025-04-02 23:20:20 -0500

    Update approval_document.py
    code

--- models/approval_document.py
+++ models/approval_document.py
@@ -1,1 +1,7 @@
+from . import db
 
+class ApprovalDocument(db.Model):
+    __tablename__ = 'approval_documents'
+    id = db.Column(db.Integer, primary_key=True)
+    pdf_data = db.Column(db.LargeBinary, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
